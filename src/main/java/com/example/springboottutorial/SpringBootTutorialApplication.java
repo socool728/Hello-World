30dedfe476962d2902201f47ef94d041e524b6e7
GJ-TECH <devope08@gmail.com>
2023-05-22 00:08:37 +0900

    Origional html file is created.

--- src/main/java/com/example/springboottutorial/SpringBootTutorialApplication.java
+++ src/main/java/com/example/springboottutorial/SpringBootTutorialApplication.java
@@ -10,13 +10,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController //SpringBootTutorialApplication class Rest controller( request handler)
 public class SpringBootTutorialApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(SpringBootTutorialApplication.class, args);
-    }
+  public static void main(String[] args) {
+    SpringApplication.run(SpringBootTutorialApplication.class, args);
+  }
 
-    @GetMapping("/hello") //Get request for /hello
-    public String sayHello(@RequestParam(value = "myName" /* web request parameter */, defaultValue = "World") String name) {
-        return String.format("Hello %s!", name);
-    }
+  @GetMapping("/hello") //Get request for /hello
+  public String sayHello(@RequestParam(value = "myName" /* web request parameter */, defaultValue = "World") String name) {
+    return String.format("Hello %s!", name);
+  }
 
 }
