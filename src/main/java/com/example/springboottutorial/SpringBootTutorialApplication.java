b0b1139e0d78cc1b359078c781745e8e8e5ba882
GJ-TECH <devope08@gmail.com>
2023-05-21 23:48:18 +0900

    comment every annotation

--- src/main/java/com/example/springboottutorial/SpringBootTutorialApplication.java
+++ src/main/java/com/example/springboottutorial/SpringBootTutorialApplication.java
@@ -7,15 +7,15 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
+@RestController //SpringBootTutorialApplication class Rest controller( request handler)
 public class SpringBootTutorialApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootTutorialApplication.class, args);
     }
 
-    @GetMapping("/hello")
-    public String sayHello(@RequestParam(value = "myName", defaultValue = "World") String name) {
+    @GetMapping("/hello") //Get request for /hello
+    public String sayHello(@RequestParam(value = "myName" /* web request parameter */, defaultValue = "World") String name) {
         return String.format("Hello %s!", name);
     }
 
